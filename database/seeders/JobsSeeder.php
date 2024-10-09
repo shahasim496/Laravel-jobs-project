14e07933ecf560f0f1bad3c2c043ceccf1096541
Asim <shahasim496@gmail.com>
2024-10-09 05:47:51 -0700

    migrtions seeding and json file data fetching

---
+++ database/seeders/JobsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class JobsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = file_get_contents(storage_path('jobs.json'));
+        $data = json_decode($json, true);
+      
+        // php artisan db:seed --class=Jobsseeder
+
+        foreach ($data as $item) {
+            DB::table('jobs')->insert([
+                'title' => $item['title'],
+                'type' => $item['type'],
+                'description' => $item['description'],
+                'salary' => $item['salary'],
+                'name' => $item['name'],
+                'cdescription' => $item['cdescription'],
+                'contactEmail' => $item['contactEmail'],
+                'contactPhone' => $item['contactPhone'],
+
+                'created_at' => now(), // Optional: set created_at and updated_at timestamps
+                'updated_at' => now(),
+            ]);
+    }
+}
+}
